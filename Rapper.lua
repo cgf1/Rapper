71e969ac7f86de1ba8abae63bd5bc99fed025749
Chris Faylor <cgf@realedsolutions.com>
2019-07-02 18:01:20 -0400

    Streamline!

--- Rapper.lua
+++ Rapper.lua
@@ -22,12 +22,15 @@ local saved = {
 }
 
 local slotn
-local mounted
-local ticking
 
 local rearm
-local reset_slot
-local try_reset
+
+local pinned = false
+
+local orig_df = df
+
+local function emptyfunc()
+end
 
 local function call(sub, x)
     return function(_, ...) sub(x, ...) end
@@ -41,133 +44,89 @@ local function israpid()
     return israpid, aid
 end
 
-local function tick(what)
-    if ticking == what then
-        return
+local function reslot(rapidify)
+    local n = GetActiveWeaponPairInfo()
+    local aid
+    if rapidify then
+	saved.pair[n] = GetSlotBoundId(slotn)
+	SlotSkillAbilityInSlot(6, 1, 2, slotn)
+	aid = saved.pair[n]
+    elseif not pinned and saved.pair[n] then
+	aid = saved.pair[n]
+	local skillType, skillLineIndex, skillIndex = GetSpecificSkillAbilityKeysByAbilityId(aid)
+	SlotSkillAbilityInSlot(skillType, skillLineIndex, skillIndex, slotn)
     end
-    if what then
-        EVENT_MANAGER:RegisterForUpdate(name, 1000, function() try_reset('updating') end)
+    if not aid then
+	df("reslot: want rapid = %s, called with no saved aid", tostring(rapidify))
     else
-	EVENT_MANAGER:UnregisterForUpdate(name)
+	df("reslot: want rapid = %s, cur %s, saved %s", tostring(rapidify), GetAbilityName(GetSlotBoundId(slotn)), GetAbilityName(aid))
     end
-    ticking = what
+    return
 end
 
-try_reset = function(x)
-    local rapid, aid = israpid()
-df("try_reset: %s", tostring(x))
-    if not IsMounted() then
-	for n in pairs(saved.pair) do
-	    reset_slot('try_reset', n)
-	end
-    elseif ticking and not israpid() then
-	local n = GetActiveWeaponPairInfo()
-	saved.pair[n] = nil
-    end
-    if ticking and not saved.pair[1] and not saved.pair[2] then
-	tick(false)
-    end
-end
-
-local function slotit(aid)
-    local skillType, skillLineIndex, skillIndex = GetSpecificSkillAbilityKeysByAbilityId(aid)
-    SlotSkillAbilityInSlot(skillType, skillLineIndex, skillIndex, slotn)
-df("slotit %d == %d: %s", GetSlotBoundId(slotn), aid, tostring(GetSlotBoundId(slotn) == aid))
-    return GetSlotBoundId(slotn) == aid
-end
-
-reset_slot = function(x, n)
-    local aid = saved.pair[n]
-    if not aid or n ~= GetActiveWeaponPairInfo() then
-	return
-    end
-    local rapid, aidr = israpid()
-df("%s: resetting rapid, was rapid = %s, current skill = %s, want skill = %s", x, tostring(rapid), GetAbilityName(aidr), GetAbilityName(aid))
-    if not israpid() then
-	saved.pair[n] = nil
-    else
-	slotit(aid)
-        tick(true)
-    end
-    rearm('reset_slot', false)
-end
-
-local function onmount(_, mounted)
+local function onmount(x, mounted)
     local rapid, aid = israpid()
     rearm('onmount', false)
     if mounted and rapid then
+	df("onmount(%s): returning because already mounted and rapid", x)
 	return
     end
-    local n = GetActiveWeaponPairInfo()
-    if not mounted and not rapid then
-	saved.pair[n] = nil
-	return
-    end
-    if not mounted then
-	reset_slot('onmount', n)
-    else
-	saved.pair[n] = aid
-	SlotSkillAbilityInSlot(6, 1, 2, slotn)
-    end
+    local r
+    df("onmount(%s): calling reslot(%s)", x, tostring(mounted))
+    reslot(mounted)
 end
 
-local armed
-rearm = function(x, n)
-df("rearm: %s: %s", x, tostring(n))
+local armed = false
+rearm = function(x, doit)
+    df("rearm: %s: %s", x, tostring(doit))
     if armed then
 	EVENT_MANAGER:UnregisterForUpdate(nameCL)
 	armed = false
     end
-    if n == false then
-	-- nothing to do
-    elseif tonumber(n) then
-	EVENT_MANAGER:RegisterForUpdate(nameCL, n, call(rearm, 'update event'))
+    if doit then
+	EVENT_MANAGER:RegisterForUpdate(nameCL, saved.duration, function() onmount('rearm', true) end)
 	armed = true
-    elseif IsMounted() then
-	onmount(_, true)
-    elseif israpid() then
-	onmount(_, false)
     end
 end
 
 local function ability_used(x, slot)
     local n = GetActiveWeaponPairInfo()
 -- d(string.format("wp %d, mounted %s, slot %d, slotn %d, saved.pair[n] %s", n, tostring(IsMounted()), slot, slotn, tostring(saved.pair[n])))
-    if IsMounted() and slot == slotn and saved.pair[n] then
-	reset_slot('ability_used', n)
-	rearm('ability_used', saved.duration)
+    if slot == slotn and IsMounted() and israpid() then
+	reslot(false)
+	rearm('ability_used', true)
     end
 end
 
 local function onslot(x, slot)
-    if slot == slotn then
-        local n = GetActiveWeaponPairInfo()
-        local name
-        if saved.pair[n] then
-            name = GetAbilityName(saved.pair[n])
-        else
-            name = 'nil'
-        end
-
-        df("%s: slot changed %d - %s, saved: %s", x, slot, GetAbilityName(GetSlotBoundId(slot)), name)
+    local n = GetActiveWeaponPairInfo()
+    if not saved.pair[n] or IsMounted() or pinned then
+	return
+    end
+    if israpid() then
+	reslot(false)	-- turn it off
+    elseif saved.pair[n] then
+	saved.pair[n] = nil
     end
 end
 
 local function keydown()
-    onmount(_, not israpid())
+    pinned = not israpid()
+    df("keydown: pinning = %s", tostring(pinned))
+    onmount('key', pinned)
 end
 
 local function onloaded(_, addon_name)
     if addon_name ~= name then
 	return
     end
+    df = emptyfunc
     KeyDown = keydown
     saved = ZO_SavedVars:NewAccountWide(name .. 'Saved', settings_version, nil, saved)
     slotn = saved.slot + 2
     EVENT_MANAGER:UnregisterForEvent(name, EVENT_ADD_ON_LOADED)
-    EVENT_MANAGER:RegisterForEvent(name, EVENT_MOUNTED_STATE_CHANGED, onmount)
-    EVENT_MANAGER:RegisterForEvent(name, EVENT_PLAYER_COMBAT_STATE, call(try_reset, 'combat state'))
-    EVENT_MANAGER:RegisterForEvent(name, EVENT_ACTIVE_WEAPON_PAIR_CHANGED, call(try_reset, 'weapon pair changed'))
+    EVENT_MANAGER:RegisterForEvent(name, EVENT_MOUNTED_STATE_CHANGED, call(onmount, 'event'))
+    EVENT_MANAGER:RegisterForEvent(name, EVENT_ACTIVE_WEAPON_PAIR_CHANGED, call(onslot, 'weapon switched'))
     EVENT_MANAGER:RegisterForEvent(name, EVENT_ACTION_SLOT_ABILITY_USED, call(ability_used, 'slot ability used'))
     EVENT_MANAGER:RegisterForEvent(name, EVENT_ACTION_SLOT_UPDATED, call(onslot, 'slot updated'))
     EVENT_MANAGER:RegisterForEvent(name, EVENT_ACTION_SLOT_STATE_UPDATED, call(onslot, 'slot state updated'))
@@ -189,7 +148,7 @@ local function onloaded(_, addon_name)
 	d(string.format("%s: using slot %d", name, saved.slot))
     end
     SLASH_COMMANDS['/perdur'] = function(n)
-	if n == '' then
+	if not n or n == '' then
 	    -- nothing to do
 	elseif tonumber(n) then
 	    saved.duration = tonumber(n) * 1000
@@ -199,6 +158,16 @@ local function onloaded(_, addon_name)
 	end
 
 	d(string.format("%s: wait %d ms before next rapids", name, saved.duration))
+    end
+    SLASH_COMMANDS['/perdebug'] = function(n)
+	if not n or n == '' then
+	    -- nothing to do
+	elseif n == 'true' or n == 'on' then
+	    df = orig_df
+	else
+	    df = emptyfunc
+	end
+	d("rapper debugging: " .. tostring(df == orig_df))
     end
 end
 
